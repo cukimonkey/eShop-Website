d783200175def11c6aaf8d0d809b7438313276f7
Ursula Armstrong <cukimonkey@gmail.com>
2023-05-12 19:05:13 +0100

    Wrap App() in Index.js by BrowserRouter, add roots to Home page, change Header to Nav

--- src/Nav.js
+++ src/Nav.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import "./Header.css";
+import "./Nav.css";
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import StorefrontIcon from '@mui/icons-material/Storefront';
 import SearchIcon from '@mui/icons-material/Search';
+import {Link} from 'react-router-dom'
 
 
-function Header() {
+function Nav() {
     return(
-        <div className='header'>
-            <div className='header_logo'>
-        <StorefrontIcon className='header_logoImage' fontSize='large'/>
-        <h2 className='header_logoTitle'>eShop</h2>
+        <div className='nav'>
+            <div className='nav_logo'>
+        <StorefrontIcon className='nav_logoImage' fontSize='large'/>
+        <h2 className='nav_logoTitle'>eShop</h2>
       </div>
 
-      <div className="header_search">
-        <input type='text' className='header_searchInput' />
-        <SearchIcon className='header_searchIcon'/>
+      <div className="nav_search">
+        <input type='text' className='nav_searchInput' />
+        <SearchIcon className='nav_searchIcon'/>
       </div>
 
-      <div className="header_nav">
+      <div className="nav_nav">
         <div className="nav_item">
           <spam className="nav_itemLineOne">Hello Guest</spam>
           <spam className="nav_itemLineTwo">Sign In</spam>
@@ -27,13 +28,17 @@ function Header() {
           <spam className="nav_itemLineOne">Your</spam>
           <spam className="nav_itemLineTwo">Shop</spam>
         </div>
+
+        <Link to="/checkout" style={{textDecoration: 'none'}}>
         <div className="nav_itemBasket">
           <ShoppingBasketIcon className='itemBasket'/>
           <spam className="nav_itemLineTwo nav_basketCount">0</spam>
         </div>
+        </Link>
+
       </div>
-        </div>
+    </div>
     )
 }
 
-export default Header
+export default Nav;
